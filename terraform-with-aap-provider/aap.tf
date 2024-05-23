6c83ff621a82684fe70cc19144112afa6d646a77
Aoki <laokiher@redhat.com>
2024-05-23 15:41:02 -0300

    improve terraform-with-aap-provider

---
+++ terraform-with-aap-provider/aap.tf
@@ -0,0 +1,36 @@
+variable "aap_controller_host" {
+  type    = string
+  default = "https://localhost:8043"
+}
+variable "aap_controller_credential_username" {
+  type = string
+  default = "admin"
+}
+variable "aap_controller_credential_password" {
+  type = string
+  sensitive = true
+}
+variable "aap_controller_inventory_id" {
+  type = number
+}
+
+provider "aap" {
+  host                 = var.aap_controller_host
+  username             = var.aap_controller_credential_username
+  password             = var.aap_controller_credential_password
+  insecure_skip_verify = true
+}
+
+resource "aap_host" "aws_host" {
+  inventory_id = var.aap_controller_inventory_id
+  name         = "aws-instance_${var.aws_instance_name}"
+  description  = "An EC2 instance created by Terraform"
+  variables    = jsonencode(aws_instance.tf_instance)
+}
+
+resource "aap_host" "gcp_host" {
+  inventory_id = var.aap_controller_inventory_id
+  name         = "gcp-instance_${var.gcp_instance_name}"
+  description  = "A GCP instance created by Terraform"
+  variables    = jsonencode(google_compute_instance.tf_instance)
+}
--- terraform-with-aap-provider/main.tf
+++ terraform-with-aap-provider/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
 
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.12.0"
     }
 
@@ -14,57 +14,4 @@ terraform {
       source = "ansible/aap"
     }
   }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "tf-demo-aws-ec2-instance-2" {
-  ami           = "ami-0005e0cfe09cc9050"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "tf-demo-aws-ec2-instance-2"
-  }
-}
-
-provider "google" {
-  region = "northamerica-northeast1"
-}
-
-resource "google_compute_instance" "tf-demo-gcp-instance-2" {
-  name         = "tf-demo-gcp-instance-2"
-  machine_type = "e2-micro"
-  zone = "northamerica-northeast1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-11-bullseye-v20240110"
-    }
-  }
-
-  network_interface {
-    network = "default"
-  }
-}
-
-provider "aap" {
-  host     = "https://localhost:8043"
-  username = "ansible"
-  password = "test123!"
-  insecure_skip_verify = true
-}
-
-resource "aap_host" "tf-demo-aws-ec2-instance-2" {
-  inventory_id = 2
-  name = "aws_instance_tf-demo-aws-ec2-instance-2"
-  description = "An EC2 instance created by Terraform"
-  variables = jsonencode(aws_instance.tf-demo-aws-ec2-instance-2)
-}
-
-resource "aap_host" "tf-demo-gcp-instance-2" {
-  inventory_id = 2
-  name = "google_compute_instance_tf-demo-gcp-instance-2"
-  description = "A GCE instance created by Terraform"
-  variables = jsonencode(google_compute_instance.tf-demo-gcp-instance-2)
 }
